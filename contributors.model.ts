92d8dca38a121af6002f5f917926daa1180e2ebe
romualdas <romualdas@romualdass-MacBook-Pro.local>
2019-05-02 12:47:10 +0300

    First commit

---
+++ contributors.model.ts
@@ -0,0 +1,10 @@
+export interface Contributors {
+	Id: number;
+	DocTopicId: number;
+	DocExampleId: number;
+	UserId: number;
+	DocContributorTypeId: number;
+	CreationDate:  string;
+	DeletionDate: string;
+	DocContributorDeletionReasonId: number;
+};
